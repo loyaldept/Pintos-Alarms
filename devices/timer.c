f3c6c1b61d95f5e3ffda1071d57baec6c305ba30
Zuhayr Abdullazhanov <115369065+loyaldept@users.noreply.github.com>
2025-10-18 03:52:22 -0400

    Add files via upload

--- devices/timer.c
+++ devices/timer.c
@@ -1,22 +1,21 @@
-
-#include "devices/timer.h"
+#include "timer.h"
 #include <debug.h>
 #include <inttypes.h>
 #include <round.h>
 #include <stdio.h>
-#include "threads/interrupt.h"
-#include "threads/synch.h"
-#include "threads/thread.h"
+#include "../threads/interrupt.h"
+#include "../threads/synch.h"
+#include "../threads/thread.h"
 
 static int64_t ticks;
 static struct list sleep_list;
-
 static struct lock sleep_list_lock;
+
 static void timer_interrupt (struct intr_frame *args UNUSED);
 
 void timer_init (void)
 {
-  intr_register_int (0x20, timer_interrupt, 0, "timer", TIMER_IRQ_PRIORITY);
+  intr_register_int (0x20, timer_interrupt, "timer", TIMER_IRQ_PRIORITY);
   lock_init (&sleep_list_lock);
   list_init (&sleep_list);
 }
@@ -48,7 +47,17 @@ void timer_sleep (int64_t ticks)
 
   current_thread->wakeup_tick = start_ticks + ticks;
   lock_acquire (&sleep_list_lock);
-  list_push_back (&sleep_list, &current_thread->elem);
+  
+  /* insert thread into sleep list sorted by wakeup time */
+  struct list_elem *e;
+  for (e = list_begin (&sleep_list); e != list_end (&sleep_list); e = list_next (e))
+    {
+      struct thread *t = list_entry (e, struct thread, elem);
+      if (t->wakeup_tick > current_thread->wakeup_tick)
+        break;
+    }
+  list_insert (e, &current_thread->elem);
+  
   lock_release (&sleep_list_lock);
   thread_block ();
 
@@ -63,6 +72,7 @@ static void thread_wakeup (void)
 
   lock_acquire (&sleep_list_lock);
 
+  /* since list is sorted by wakeup time, we can stop at first thread not ready to wake */
   e = list_begin (&sleep_list);
   while (e != list_end (&sleep_list))
     {
@@ -74,7 +84,8 @@ static void thread_wakeup (void)
         }
       else
         {
-          e = list_next (e);
+          /* since list is sorted, no more threads need to be woken */
+          break;
         }
     }
 
@@ -83,20 +94,22 @@ static void thread_wakeup (void)
 
 void timer_mdelay (int64_t msec)
 {
-  int64_t ticks = msec_to_ticks (msec);
-  real_time_sleep (ticks);
+  int64_t ticks = msec * TIMER_FREQ / 1000;
+  timer_sleep (ticks);
 }
+
 void timer_udelay (int64_t usec)
 {
-  int64_t ticks = usec_to_ticks (usec);
-  real_time_sleep (ticks);
+  int64_t ticks = usec * TIMER_FREQ / 1000000;
+  timer_sleep (ticks);
 }
 
 void timer_ndelay (int64_t nsec)
 {
-  int64_t ticks = nsec_to_ticks (nsec);
-  real_time_sleep (ticks);
+  int64_t ticks = nsec * TIMER_FREQ / 1000000000;
+  timer_sleep (ticks);
 }
+
 void timer_print_stats (void)
 {
   printf ("Timer: %"PRId64" ticks\n", timer_ticks ());
@@ -108,13 +121,3 @@ static void timer_interrupt (struct intr_frame *args UNUSED)
   thread_wakeup (); /* check for sleeping threads to wake up. */
   thread_yield ();
 }
-
-/* busy-waits in the timer interrupt handler. */
-static void real_time_sleep (int64_t num_ticks)
-{
-  int64_t start_ticks = timer_ticks ();
-  while (timer_elapsed (start_ticks) < num_ticks)
-    thread_yield ();
-}
-
-
--- devices/timer.h
+++ devices/timer.h
@@ -1,25 +1,15 @@
-/* devices/timer.h - System timer. */
-
 #ifndef DEVICES_TIMER_H
 #define DEVICES_TIMER_H
 
-#include <round.h>
 #include <stdint.h>
 
 /* number of timer interrupts per second. */
 #define TIMER_FREQ 100
 
-void timer_init (void);
-void timer_calibrate (void);
+void timer_init(void);
+int64_t timer_ticks(void);
+int64_t timer_elapsed(int64_t start_tick);
+void timer_sleep(int64_t ticks);
+void timer_print_stats(void);
 
-int64_t timer_ticks (void);
-int64_t timer_elapsed (int64_t);
-
-void timer_sleep (int64_t ticks);
-void timer_msleep (int64_t msec);
-void timer_usleep (int64_t usec);
-void timer_nsleep (int64_t nsec);
-
-void timer_print_stats (void);
-
-#endif /* devices/timer.h */
+#endif /* DEVICES_TIMER_H */
